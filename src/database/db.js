a0adfba79a8d466bea94fbc081bc28155cf8e8c3
yedulima <eduardolimacmj2017@gmail.com>
2025-06-06 22:38:26 -0300

    fix: corrigir erros no db.js e refatoração no UserSchema

--- src/database/db.js
+++ src/database/db.js
@@ -8,7 +8,6 @@ const connectDatabase = () => {
 
     mongoose.connect(
         process.env.DB_URL,
-        { useNewUrlParser: true, useUnifiedTopology: true, }
     )
     .then(() => console.log("MongoDB Atlas connected."))
     .catch((error) => console.log(error));
--- src/models/User.js
+++ src/models/User.js
@@ -12,11 +12,13 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true,
     },
     password: {
         type: String,
         required: true,
+        select: false,
     },
     avatar: {
         type: String,
