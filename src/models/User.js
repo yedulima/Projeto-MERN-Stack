0a913838caee4a5353c79e390b4117ca94124107
yedulima <eduardolimacmj2017@gmail.com>
2025-06-07 13:35:04 -0300

    refactor: adicionar criptografia a senha e remodelagem do schema de usuários

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -8,6 +9,7 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
     },
     email: {
         type: String,
@@ -28,6 +30,24 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+});
+
+UserSchema.pre("save", async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+UserSchema.pre("findOneAndUpdate", async function (next) {
+    const update = this.getUpdate();
+
+    if (update.password) {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(update.password, salt);
+        update.password = hash;
+        this.setUpdate(update);
+    };
+
+    next();
 });
 
 const User = mongoose.model("User", UserSchema);
