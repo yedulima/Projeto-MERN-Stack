c213782cf064496ab869536a87fb44435a92e725
yedulima <eduardolimacmj2017@gmail.com>
2025-06-11 16:10:43 -0300

    fix: corrigir erro na rota /posts/findAll no render

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -6,16 +6,16 @@ import { validPost } from "../middlewares/post.middleware.js";
 
 const router = Router();
 
-router.post("/", authMiddleware, create);
+router.post("/create", authMiddleware, create);
 
-router.get("/", findAll);
+router.get("/findAll", findAll);
 router.get("/top", topPosts);
 router.get("/search", searchByTitle);
 router.get("/byUserId", authMiddleware, searchByUserId);
 
 router.get("/:id", authMiddleware, validId, validPost, findById);
 router.patch("/update/:id", authMiddleware, validId, validPost, update);
-router.delete("delete//:id", authMiddleware, validId, validPost, exclude);
+router.delete("delete/:id", authMiddleware, validId, validPost, exclude);
 router.patch("/like/:id", authMiddleware, validId, validPost, like);
 router.patch("/comment/:id", authMiddleware, validId, validPost, addComment);
 router.patch("/comment/:id/:idComment", authMiddleware, validId, validPost, deleteComment)
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,9 +4,9 @@ import { validId, validUser } from "../middlewares/global.middlewares.js";
 
 const router = Router();
 
-router.post("/", create);
+router.post("/create", create);
 
-router.get("/", findAll);
+router.get("/findAll", findAll);
 
 router.get("/:id", validId, validUser, findById);
 router.patch("/update/:id", validId, validUser, update);
